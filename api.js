c76e9d1d52141dc60216de853dfe592b7060c7d9
aditya <adityafernanda2013@gmail.com>
2020-08-14 22:56:20 +0700

    RestAPI_MMySQL

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const mysql = require("mysql")
+const moment = require("moment")
 
 //implementasi nya
 const app =  express()
@@ -12,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 //membuat mysql connection
 const db = mysql.createConnection({
-    host: "localthost",
+    host: "localhost",
     user: "root",
     password: "",
     database: "pelanggaran_siswa"
@@ -82,7 +83,7 @@ app.post("/siswa", (req,res)=>{
         poin: req.body.poin
     }
     //buat sql query insert
-    let sql = "inser into siswa set ?"
+    let sql = "insert into siswa set ?"
 
     //run query
     db.query(sql, data, (error, result)=>{
@@ -158,6 +159,49 @@ app.delete("/siswa/:id", (req,res)=>{
             }
         }
         res.json(response) //kirim response
+    })
+})
+
+//endpoint tambah data pelanggaran siswa
+app.post("/pelanggaran_siswa", (req,res)=>{
+    //prepeara data to pelanggaran siswa
+    let data = {
+        id_siswa: req.body.id_siswa,
+        id_user: req.body.id_user,
+        waktu: moment().format('YYYY-MM-DD HH:mm:ss')// gt current time
+    }
+    //parse => json
+    let pelanggaran = JSON.parse(req.body.pelanggaran)
+
+    //buat query insert to pelanggaran siswa
+    let sql = "insert into pelanggaran_siswa set ?"
+
+    //run
+    db.query(sql, data, (error, result)=>{
+        let response = null
+        if (error) {
+            res.json({message:error.message})
+        } else {
+            //get last insert id_pelanggaran
+            let lastID = result.insertID
+
+            //prepare datda to detail pelanggarna
+            let data = []
+            for (let index = 0; index < pelanggaran.length; index++) {
+                data.push([
+                    lastID, pelanggaran[index].id_pelanggaran
+                ])
+            }
+            //buat query insert detain pelanggaran
+            let sql = "insert into detail_pelanggaran_siswa values ?"
+            db.query(sql, [data], (error, result) =>{
+                if (error) {
+                    res.json({message: error.message})
+                } else {
+                    res.json({message: "Data has been inserted"})
+                }
+            })
+        }
     })
 })
 
